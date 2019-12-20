528c105b3fe852344da098656fd5312407c40d69
firmo holanda <firmo@viize.com>
2019-12-20 20:15:15 -0300

    removed comments

--- bin/twitter_bot.rb
+++ bin/twitter_bot.rb
@@ -96,15 +96,3 @@ class TwitterBot
   end
 
 end
-
-
-my_bot = TwitterBot.new	
-
-# my_bot.find_tweets(topics_to_search, display_wile_searching, like_the_tweet, alert_in_new_tweet)	
-my_bot.find_tweets(["trump"], true, false, false)	
-my_bot.display_top_10_locations	
-my_bot.save_stored_tweets	
-
-
-#my_bot.retrieve_stored_tweets	
-#my_bot.display_stored_tweets
