1018ff35e4ec08e7038167f4e2497b5ecfc862c4
firmo holanda <firmo@viize.com>
2020-01-01 14:10:57 -0300

    Update twitter_bot.rb

--- bin/twitter_bot.rb
+++ bin/twitter_bot.rb
@@ -20,10 +20,10 @@ class TwitterBot
 
   def connect_to_api
     @@api_config = {
-        consumer_key:        "niJqBHvcmyePEiiD1LHFI1HnO",
-        consumer_secret:     "ug2mObU0N2NQ0RzSSlF5J0w0QOliFmdDjQA7FWYd4zKk2qWjit",
-        access_token:        "25709895-Qy9pZeLoOXGHZT9D3C1LNKZcLDc0DbqQpkBjooLka",
-        access_token_secret: "74AA4gcJDMbNfExZhBEE3XeCDlJhwnnKaga51wU9nTr2p"
+        consumer_key:        "",
+        consumer_secret:     "",
+        access_token:        "",
+        access_token_secret: ""
     }
     @@r_client = Twitter::REST::Client.new @@api_config
     @@s_client = Twitter::Streaming::Client.new(@@api_config)
